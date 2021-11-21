381e4254a3494d779b6e5c5bda2528c6331cab33
Jonathan Bessette <JOBES10@ulaval.ca>
2021-11-21 00:39:34 -0800

    Header reduces on scroll / darkModeToggle speed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,6 +8,8 @@ var canAnimate = true;
 // For some reason, couldn't use a hook in the function Header().
 // Might want to check that out later.
 
+const DELTA_HEADER = 150;
+
 const HeaderFlex = styled.div`
     transition: all 0.5s;
     background-color: ${props => props.theme.headerBackground}
@@ -17,6 +19,8 @@ function Header(props) {
 
     const [isDarkMode, setIsDarkMode] = useState(true);
     const [burgerOpened, setBurgerOpened] = useState(false);
+    const [headerClasses, setHeaderClasses] = useState("headerExpand");
+    var didScroll = false;
 
 
     function animateButton() {
@@ -30,8 +34,32 @@ function Header(props) {
         setBurgerOpened(!burgerOpened);
     }
 
+    setInterval(() => {
+        if(didScroll) {
+            didScroll = false;
+            onScrollEvent();
+        }
+    }, 250);
+
+    var expanded = false;
+    var ST;
+    function onScrollEvent() {
+        ST = window.scrollY;
+        if(ST >= DELTA_HEADER && !expanded) {
+            expanded = true;
+            setHeaderClasses("headerReduce");
+        } else if (ST < DELTA_HEADER && expanded) {
+            expanded = false;
+            setHeaderClasses("headerExpand");
+        }
+    }
+
+    window.addEventListener('scroll', () => {
+        if(!didScroll) didScroll = true;
+    });
+
     return (
-        <HeaderFlex className="header">
+        <HeaderFlex className={`header ${headerClasses}`}>
             <div className="burger-flex">
                 <Burger sendBurgerState={sendBurgerState} theme={props.theme}/>
                 <Linker burgerOpened={burgerOpened}/>
@@ -48,7 +76,7 @@ function Header(props) {
                 }}
                 checked={isDarkMode}
                 size={70}
-                speed={2}
+                speed={2.6}
             />
         </HeaderFlex>
     );
